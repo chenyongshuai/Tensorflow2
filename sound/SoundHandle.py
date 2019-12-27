f24de3e7b8182d1725647ab322aa2849a14d7284
chenyongshuai <public_chenys@163.com>
2019-12-27 17:05:04 +0800

    语音处理模板20191227

--- sound/SoundHandle.py
+++ sound/SoundHandle.py
@@ -6,6 +6,7 @@ import wave as we
 from scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 logger = logging.getLogger('__name__.SoundHandle')
 class SoundHandle:
@@ -47,16 +48,15 @@ class SoundHandle:
                 logging.info("WAVE文件名：" + path)
             WAVE = we.open(path)
             sum_sample = WAVE.getparams().nframes
-            logging.info("sum_sample：%s" %sum_sample)
             sample_frequency, audio_sequence = wavfile.read(path)
-            logging.info("sample_frequency：%s - audio_sequence：%s" % (sample_frequency, audio_sequence))
-            x_seq = np.arange(0, sum_sample/sample_frequency, 1/sample_frequency)
+            logging.info("sum_sample：%s - sample_frequency：%s - audio_sequence：%s" % (sum_sample,sample_frequency, audio_sequence))
+            #x_seq = np.arange(0, sum_sample/sample_frequency, 1/sample_frequency)
         except FileNotFoundError as f:
             logging.error("输入的文件路径不存在：" + path)
             traceback.print_exc()
         finally:
             return sum_sample, sample_frequency, audio_sequence
-    def showWave(self,x,y):
+    def showWave(self,x,y):#展示Wav文件图谱
         """
         :param x: 分成多少份
         :param y: 每一份的值
@@ -75,13 +75,78 @@ class SoundHandle:
             traceback.print_exc()
         finally:
             return
+    def normalization(self,data):#标准化处理
+        """
+        :param data: 音频文件的数据内容
+        方法： MaxAbs
+        :return: 标准化后的数据内容
+        """
+        try:
+            data_R = preprocessing.MaxAbsScaler().fit_transform(data)
+        except Exception as e:
+            #traceback.print_exc()
+            data = data.reshape(-1, 1)
+            data_R = preprocessing.MaxAbsScaler().fit_transform(data)
+            data_R = data_R.reshape(-1)
+        finally:
+            logger.info("标准化前的shape：%s 数据：%s " % (data.shape, data))
+            logger.info("标准化后的shape：%s 数据：%s " % (data_R.shape, data_R))
+            return data_R
     def preEmphasis(self,data):#预加重处理
         """
         :param data：音频文件的数据内容
         方法：Y(n) = X(n) - a * X(n-1)  ### a = 0.9375
         :return: data：预加重后的数据内容
         """
-        print()
+        try:
+            logger.info("预加重前的shape：%s 数据：%s " %(data.shape,data))
+            a = 0.9375
+            data_R = []
+            data_R.append(data[0])
+            for i in range (1,len(data)):
+                r = data[i] - a*data[i-1]
+                data_R.append(r)
+            data_R = np.asarray(data_R)
+            logger.info("预加重后的shape：%s 数据：%s " %(data_R.shape,data_R))
+        except AttributeError as a:
+            logger.error(a)
+            traceback.print_exc()
+        return data_R
+    def doFraming(self,data,frequency,one_frame_time:float=None,overlap_time:float=None):
+        """
+        :param data:
+        :param frame_length:
+        :param overlap_length:
+        :return:
+        """
+        try:
+            self.one_frame_time = one_frame_time if one_frame_time is not None else 25.
+            self.overlap_time = overlap_time if overlap_time is not None else 10.
+            one_frame_num = self.one_frame_time / ( 1. / float(frequency) *1000 )
+            overlap_num = self.overlap_time / ( 1. / float(frequency) *1000 )
+            logger.info("一帧样本点个数：%s , 帧移样本点数：%s" %(one_frame_num,overlap_num))
+            frame_array = []
+            num_frame=0
+            if len(data) <= one_frame_num:
+                num_frame=1
+            else:
+                num_frame = int(np.ceil((1.0 * len(data) - one_frame_num + overlap_num ) / overlap_num))
+            logger.info("总帧数：%s " % (num_frame))
+            pad_length = int((num_frame - 1) * overlap_num + one_frame_num)
+            logger.info("所有帧加起来总的铺平后的长度：%s " % (pad_length))
+            zeros = np.zeros((pad_length - len(data),))
+            logger.info("填补长度：%s " % (zeros.shape))
+            pad_data = np.concatenate((data, zeros))
+            logger.info("填补前的信号长度：%s - 填补后的信号长度：%s " % (data.shape,pad_data.shape))
+        except Exception as e:
+            print(e)
+        finally:
+            print()
+
+
+
+
+
 
 
 
--- sound/main.py
+++ sound/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-
+import numpy as np
 from sound.SoundHandle import SoundHandle
 
 logging.basicConfig(level=logging.INFO,
@@ -8,6 +8,12 @@ logging.basicConfig(level=logging.INFO,
                    datefmt='%a, %d %b %Y %H:%M:%S',
                     filename='log.txt')
 logger = logging.getLogger(__name__)
-path = "D:/python/workspaces/Tensorflow2.0/sound/enen.a"
+path = "E:/python/Tensorflow2.0/sound/enen.wav"
 sh = SoundHandle()
-sh.transformWave(path)
+sum_sample, sample_frequency, audio_sequence = sh.readWaveFile(path)
+data_R = sh.normalization(audio_sequence)
+x_seq = np.arange(0, sum_sample/sample_frequency, 1/sample_frequency)
+data_R = sh.preEmphasis(data_R)
+sh.doFraming(data_R,sample_frequency)
+#sh.showWave(x_seq,data_R)
+
